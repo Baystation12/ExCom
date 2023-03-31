b896e1a92924a90381924a4dec83d7a9e4a383dd
Mucker <1161516+MuckerMayhem@users.noreply.github.com>
2023-03-31 11:57:33 -0700

    pm cmd, ahelp relaying, misc updates

--- config.js
+++ config.js
@@ -1,1 +1,10 @@
+///Discord bot token, obtained from https://discord.com/developers/applications
 export const token = ''
+///The servers comms key, used to securely access some topics
+export const key = ''
+///The IP address that the HTTP server will listen on, which is for listening for data from the server
+export const listen_host = ''
+///The port that the HTTP server will listen on
+export const listen_port = ''
+///The discord guild snowflake
+export const guild_id = ''
